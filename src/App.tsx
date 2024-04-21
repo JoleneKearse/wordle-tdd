0c98efa9eea53c2f183988a371935d880158b3ca
JoleneKearse <pharaohnutz@gmail.com>
2024-04-21 17:29:36 -0400

    chore: add keyboard variable

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,9 @@ import Header from "./components/Header";
 
 function App() {
   return (
-    <>
+    <main className="bg-neutral-100 h-screen dark:bg-neutral-900">
       <Header />
-    </>
+    </main>
   );
 }
 
--- src/wordle.ts
+++ src/wordle.ts
@@ -81,3 +81,5 @@ export const makeGuess = (guess: string, game: Game) => {
     guessesRemaining: guess  === game.answer ? 0 : game.guessesRemaining === 0 ? 0 : game.guessesRemaining - 1,
   };
 };
+
+export const keyboard: string[] = ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "a", "s", "d", "f", "g", "h", "j", "k", "l", "z", "x", "c", "v", "b", "n", "m"];
