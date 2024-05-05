90d6fe8e293847d7dfdff94e3e67e3e899e42bbc
JoleneKearse <pharaohnutz@gmail.com>
2024-05-05 15:36:25 -0400

    chore: make each input uppercase

--- src/components/Grid.tsx
+++ src/components/Grid.tsx
@@ -6,31 +6,31 @@ const Grid = () => {
           type="text"
           name=""
           id="firstGuessLetter1"
-          className="flex items-center justify-center w-12 py-3 pl-4 text-3xl font-bold border border-2 rounded-md border-neutral-600 bg-neutral-100 dark:bg-neutral-900 text-neutral-900 dark:text-neutral-300 focus-visible:ring-[#4a1837]"
+          className="flex items-center justify-center w-12 py-3 pl-4 text-3xl font-bold border border-2 rounded-md border-neutral-600 bg-neutral-100 dark:bg-neutral-900 text-neutral-900 dark:text-neutral-300 focus-visible:ring-[#4a1837] uppercase"
         />
         <input
           type="text"
           name=""
           id="firstGuessLetter2"
-          className="flex items-center justify-center w-12 py-3 pl-4 text-3xl font-bold border border-2 rounded-md border-neutral-600 bg-neutral-100 dark:bg-neutral-900 text-neutral-900 dark:text-neutral-300"
+          className="flex items-center justify-center w-12 py-3 pl-4 text-3xl font-bold uppercase border border-2 rounded-md border-neutral-600 bg-neutral-100 dark:bg-neutral-900 text-neutral-900 dark:text-neutral-300"
         />
         <input
           type="text"
           name=""
           id="firstGuessLetter3"
-          className="flex items-center justify-center w-12 py-3 pl-4 text-3xl font-bold border border-2 rounded-md border-neutral-600 bg-neutral-100 dark:bg-neutral-900 text-neutral-900 dark:text-neutral-300"
+          className="flex items-center justify-center w-12 py-3 pl-4 text-3xl font-bold uppercase border border-2 rounded-md border-neutral-600 bg-neutral-100 dark:bg-neutral-900 text-neutral-900 dark:text-neutral-300"
         />
         <input
           type="text"
           name=""
           id="firstGuessLetter4"
-          className="flex items-center justify-center w-12 py-3 pl-4 text-3xl font-bold border border-2 rounded-md border-neutral-600 bg-neutral-100 dark:bg-neutral-900 text-neutral-900 dark:text-neutral-300"
+          className="flex items-center justify-center w-12 py-3 pl-4 text-3xl font-bold uppercase border border-2 rounded-md border-neutral-600 bg-neutral-100 dark:bg-neutral-900 text-neutral-900 dark:text-neutral-300"
         />
         <input
           type="text"
           name=""
           id="firstGuessLetter5"
-          className="flex items-center justify-center w-12 py-3 pl-4 text-3xl font-bold border border-2 rounded-md border-neutral-600 bg-neutral-100 dark:bg-neutral-900 text-neutral-900 dark:text-neutral-300"
+          className="flex items-center justify-center w-12 py-3 pl-4 text-3xl font-bold uppercase border border-2 rounded-md border-neutral-600 bg-neutral-100 dark:bg-neutral-900 text-neutral-900 dark:text-neutral-300"
         />
       </div>
 
