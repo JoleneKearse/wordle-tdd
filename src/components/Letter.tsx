829d68001b6d3f60aa7e2e4f670bb7e278d23a81
JoleneKearse <pharaohnutz@gmail.com>
2024-05-17 17:47:13 -0400

    chore: start backend

--- src/components/Letter.tsx
+++ src/components/Letter.tsx
@@ -1,9 +1,39 @@
+import React, { useRef, useEffect } from "react";
+
 type LetterProps = {
   id: string;
+  autoFocus?: boolean;
 };
-const Letter = ({ id }: LetterProps): JSX.Element => {
+
+const Letter = ({ id, autoFocus }: LetterProps): JSX.Element => {
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    if (autoFocus) {
+      inputRef.current?.focus();
+    }
+  }, [autoFocus]);
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = event.target;
+    if (value.match(/^[a-zA-Z]$/)) {
+      event.target.value = value.toUpperCase();
+      const nextLetter = inputRef.current
+        ?.nextElementSibling as HTMLInputElement;
+      if (nextLetter && nextLetter.tagName === "INPUT") {
+        nextLetter.focus();
+      }
+    } else {
+      event.target.value = "";
+    }
+  };
+
   return (
     <input
+      ref={inputRef}
+      maxLength={1}
+      onChange={handleChange}
+      autoComplete="off"
       type="text"
       name=""
       id={id}
